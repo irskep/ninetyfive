0aedcb544525c10af6836fffbc2d1174d4a055fb
Steve Johnson <steve@steveasleep.com>
2019-02-23 12:23:46 -0800

    Try to kill some key warnings

--- src/StaticWindow.jsx
+++ src/StaticWindow.jsx
@@ -4,12 +4,12 @@ import ___ from "./ui.scss";
 export default function StaticWindow({title, children, windowStyle, titleExtra}) {
   return (
     <div className="W95__Window m-static">
-      <div className="W95__WindowBG" />
-      <div className="W95__WindowTitle">
+      <div className="W95__WindowBG" key="bg" />
+      <div className="W95__WindowTitle" key="title">
         {title}
         {titleExtra}
       </div>
-      <div className="W95__WindowContents" style={windowStyle || {}}>
+      <div className="W95__WindowContents" key="contents" style={windowStyle || {}}>
         {children}
       </div>
     </div>
